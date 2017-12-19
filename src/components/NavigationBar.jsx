c6ade39e42a9d88716b18ef00dd2a17e579ce585
justinjyoo <justinjyoo@gmail.com>
2017-12-18 23:00:33 -0800

    Pull elements out of ul
    <ul> should only contain <li> elements
    
    Fix css as a result

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,18 +3,19 @@ import './NavigationBar.css'
 
 class NavigationBar extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       signedIn: false,
-      modalStyle: {display: 'block'}
+      modalStyle: {display: 'none'},
+      SignUpInformation: {}
     }
 
     this.displayModal = this.displayModal.bind(this)
     this.hideModal = this.hideModal.bind(this)
   }
 
-  componentDidMount () {
+  componentDidMount() {
     window.addEventListener('mousedown', this.hideModal)
   }
 
@@ -28,40 +29,47 @@ class NavigationBar extends Component {
       this.setState({modalStyle: {display: 'none'}})
       console.log('hideModal', this.state.modalStyle)
     }
+  }
 
+  handleSignUpSubmit(event) {
+    event.preventDefault();
   }
 
   onClickEmptyPlaceholder(event) {
     event.target.placeholder = ""
   }
 
-  render(){
+  render() {
     return (
-      <div className="App">
+    <div>
+      <div className="nav-bar">
         <ul>
-            <li><a href="default">Home</a></li>
-            <li><a href="about">About</a></li>
-            <li><a href="blog">Blog</a></li>
-            <li><a href="contact">Contact</a></li>
-            <input class="auth" type="button" value="Sign Up" onClick={this.displayModal}/ >
-            <input class="auth" type="button" value="Sign In" />
-            <div id="myModal" className="modal" style={this.state.modalStyle}>
-              <div className="model-content">
-                <div className="modal-body">
-                  <input placeholder="First Name" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "First Name"} />
-                  <input placeholder="Last Name" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "Last Name"} />
-                  <input placeholder="Email" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "Email"} />
-                  <input placeholder="Mobile Number" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "Mobile Number"} />
-                  <input placeholder="New Password" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "New Password"} />
-                  <input class="submit-button" type="submit" value="Submit" />
-                </div>
-                <span className="close" onClick={this.hideModal} >&times;</span>
-              </div>
-            </div>
+          <li><a href="default">Home</a></li>
+          <li><a href="about">About</a></li>
+          <li><a href="blog">Blog</a></li>
+          <li><a href="contact">Contact</a></li>
         </ul>
+        <input className="auth" type="button" value="Sign Up" onClick={this.displayModal} />
+        <input className="auth" type="button" value="Sign In" />
       </div>
+      <div id="myModal" className="modal" style={this.state.modalStyle}>
+        <div className="model-content">
+          <div className="modal-body">
+            <form onSubmit={this.handleSignUpSubmit}>
+              <input placeholder="First Name" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "First Name"} />
+              <input placeholder="Last Name" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "Last Name"} />
+              <input placeholder="Email" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "Email"} />
+              <input placeholder="Mobile Number" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "Mobile Number"} />
+              <input placeholder="New Password" onFocus={this.onClickEmptyPlaceholder}  onBlur={(e) => e.target.placeholder = "New Password"} />
+              <input className="submit-button" type="submit" value="Submit" />
+            </form>
+          </div>
+          <span className="close" onClick={this.hideModal} >&times;</span>
+        </div>
+      </div>
+    </div>
     )
   }
 }
 
-export default NavigationBar
+export default NavigationBar;
---
+++ src/components/SignUpModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class SignUpModal extends React.Component {
+
+}
+
+export default SignUpModal;
